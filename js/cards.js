b41b95ea72b24117b3eaadbc8bdc8300c56f8902
Jim Hooper <jim@massrelevance.com>
2015-02-23 01:52:43 -0600

    Fix Cards

--- js/cards.js
+++ js/cards.js
@@ -1,8 +1,25 @@
 define(["jquery","snabbt"], function($, snabbt)
 {
   
-  
-  function Deck(){
+  var imgdata = [
+       'img/websites/ahha.jpg',
+       'img/websites/caravan.jpg',
+        'img/websites/construction.jpg',
+        'img/websites/english.jpg',
+        'img/websites/fudge.jpg',
+        'img/websites/totelcom.jpg',
+        'img/websites/wafresh.jpg',
+        'img/websites/desires.jpg',
+        'img/websites/dlprint.jpg',
+        'img/websites/greensplus.jpg',
+        'img/websites/halosleep.jpg',
+        'img/websites/holycross.jpg',
+        'img/websites/threeby.jpg',
+        'img/websites/turtle.jpg',
+        'img/websites/wildseed.jpg'
+         
+  ];
+  function Deck(imgdata){
     this.cards = [];
     this.card_index = [];
     this.$container = $('#surface');
@@ -11,10 +28,15 @@ define(["jquery","snabbt"], function($, snabbt)
     this.create_card = function(index){
        
       var card = document.createElement('div');
+      var imgEl = document.createElement('img');
       card.className = 'card';
       card.style.background = 'red';
-
+       
+      imgEl.setAttribute('src',imgdata[index]);
+      card.appendChild(imgEl);
+        
       this.$container.append(card);
+
       return card;
    
     }
@@ -31,7 +53,7 @@ define(["jquery","snabbt"], function($, snabbt)
     this.reset = function() {
       this.card_index = 0;
     };
-    for(var i=0;i<29;++i) {
+    for(var i=0;i<30;++i) {
       
       this.cards.push(this.create_card(i));
     }
@@ -40,23 +62,13 @@ define(["jquery","snabbt"], function($, snabbt)
   
  
   function CardViz(){
-    this.Deck = new Deck();
+    this.Deck = new Deck(imgdata);
     this.config = {
-        CARD_HEIGHT : 100,
-        CARD_WIDTH : 60,
-        CARD_COUNT : 29,
-        
-        WIDTH : 800,
-        HEIGHT : 600,
-        BOTTOM : 400,
-
-        TILT : Math.PI/8,
-        PYTH_ANGLE : Math.PI/2 - this.TILT,
-
-        TILTED_CARD_HEIGHT : Math.sin(this.PYTH_ANGLE) * this.CARD_HEIGHT + 2,
-        TILTED_CARD_WIDTH : Math.cos(this.PYTH_ANGLE) * this.CARD_HEIGHT,
-        CARD_SPACING : 2
- 
+        CARD_HEIGHT : 125,
+        CARD_WIDTH : 300,
+        CARD_COUNT : 30,
+        WIDTH : 500,
+        HEIGHT : 500, 
     }
      
     return this;
@@ -64,12 +76,11 @@ define(["jquery","snabbt"], function($, snabbt)
 
   CardViz.prototype.update_sizes = function() {
         this.container = document.getElementById('cylinder-container');
-        WIDTH = this.container.clientWidth;
-        HEIGHT = this.container.clientHeight;
-        CARD_WIDTH = WIDTH * 0.05;
-        CARD_HEIGHT = HEIGHT * 0.15;
-        TILTED_CARD_HEIGHT = Math.sin(this.config.PYTH_ANGLE) * CARD_HEIGHT + 2;
-        TILTED_CARD_WIDTH = Math.cos(this.config.PYTH_ANGLE) * CARD_HEIGHT;
+        this.config.WIDTH = this.container.clientWidth;
+        this.config.HEIGHT = this.container.clientHeight;
+        CARD_WIDTH = this.config.WIDTH * 0.10;
+        CARD_HEIGHT = this.config.HEIGHT * 0.15;
+         
         
         for(var i=0;i<this.Deck.cards.length;++i) {
           this.Deck.card_at(i).style.height = CARD_HEIGHT + 'px';
@@ -80,25 +91,27 @@ define(["jquery","snabbt"], function($, snabbt)
  
   CardViz.prototype.build_formation = function(positions) {
      this.Deck.reset();
-     window.console.log(positions);
+      
     for(i=0;i<positions.length;++i) {
       snabbt(this.Deck.next_card(), {
         position: positions[i].position,
         rotation: positions[i].rotation,
         easing: 'ease',
-        delay: i * 50
+        delay: i * 100
       });
     }
   };
    
   CardViz.prototype.rotate_container = function() {
     var container = document.getElementById('surface');
-    snabbt(container, {
+    var it = snabbt(container, {
       rotation: [0, 2*Math.PI, 0],
-      duration: 10000,
+      duration: 20000,
       perspective: 2000,
       loop: Infinity
     });
+    
+     
   };
   CardViz.prototype.pile_positions = function() {
      this.Deck.reset();
@@ -106,11 +119,11 @@ define(["jquery","snabbt"], function($, snabbt)
 
       var i = 0;
       var card = this.Deck.next_card();
-      var center = (WIDTH - CARD_WIDTH)/2;
-      var y = HEIGHT - HEIGHT*0.2;
+      var center = (this.config.WIDTH - this.config.CARD_WIDTH)/2;
+      var y = this.config.HEIGHT - this.config.HEIGHT*0.2;
       while(card) {
         positions.push({
-          position: [center, y - i*0.5, WIDTH*0.1],
+          position: [center, y - i*0.5, this.config.WIDTH*0.1],
           rotation: [Math.PI/2, 0, 0],
         });
         ++i;
@@ -120,9 +133,9 @@ define(["jquery","snabbt"], function($, snabbt)
   };
   CardViz.prototype.cylinder_positions = function() {
     var positions = [];
-    var start_x = WIDTH / 2;
-    var start_y = HEIGHT * 0.1;
-    var radius = WIDTH*0.2;
+    var start_x = this.config.WIDTH / 2.2;
+    var start_y = this.config.HEIGHT * 0.1;
+    var radius = this.config.WIDTH*0.2;
     for(var i=0;i<this.config.CARD_COUNT;++i) {
       var angle = ((i % 10) / 10) * 2 * Math.PI;
       var x = Math.cos(angle) * radius + start_x;
