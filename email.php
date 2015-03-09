8edf051b843154dbf0493bcfc1c3a1390264f343
Jim Hooper <jim@massrelevance.com>
2015-03-09 02:20:24 -0500

    Add better fonts/style

--- email.php
+++ email.php
@@ -1,14 +1,8 @@
  <?php
-
- 
 	$msg = 'Name: ' . $_POST['name'] . "\n" 
 		. 'Email: ' . $_POST['email'] . "\n"
 		. 'Comment: ' . $_POST['comment'];
 		@mail('jimhooper86@gmail.com', 'JimHooper.me Email', $msg);
-		 
-	
- 
-
 ?>
  
  
