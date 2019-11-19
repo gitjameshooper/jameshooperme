6337c30e44810cd86cedb3ef4126d616553690fc
gitjameshooper <gitjameshooper@github.com>
2019-11-18 20:12:33 -0600

    Update Templates and Content

--- email.php
+++ email.php
@@ -2,7 +2,7 @@
 	$msg = 'Name: ' . $_POST['name'] . "\n" 
 		. 'Email: ' . $_POST['email'] . "\n"
 		. 'Comment: ' . $_POST['comment'];
-		@mail('jimhooper86@gmail.com', 'James-Hooper.me Email', $msg);
+		@mail('jamesthomashooper@gmail.com', 'James-Hooper.me Email', $msg);
 ?>
  
  
